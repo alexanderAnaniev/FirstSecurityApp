b632a16fe923a97fd4c4a0ece8a8fbf704daf02d
sk1nwalk3r <ananievsalexander@gmail.com>
2023-02-01 12:43:07 +0300

    zz

--- src/main/java/ru/youlola/spring/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/youlola/spring/FirstSecurityApp/config/SecurityConfig.java
@@ -1,26 +1,18 @@
 package ru.youlola.spring.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import ru.youlola.spring.FirstSecurityApp.services.PersonDetailsService;
 
-import java.util.HashMap;
-import java.util.Map;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+@Configuration
+public class SecurityConfig  {
 
     private final PersonDetailsService personDetailsService;
 
@@ -31,16 +23,21 @@ public class SecurityConfig extends WebSecurityConfiguration {
     }
 
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(personDetailsService);
+    @Bean
+    public AuthenticationManager configure(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
+        return authenticationManagerBuilder.build();
     }
 
 
     @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public  PasswordEncoder passwordEncoder() {
+        return  NoOpPasswordEncoder.getInstance();
     }
 }
+
 
 
 
